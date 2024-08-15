3695ec67d3e4be341232a7b27f1341335f642419
Renan <renanalmeida201612@gmail.com>
2024-08-14 23:12:03 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+interface ItemsInterface {
+  id: number;
+  date: string;
+  price: string;
+  payment: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'naribrecho';
+  priceValue = '';
+  items: ItemsInterface[] = [
+    {
+      id: 1,
+      date: '14/08/24',
+      price: '14',
+      payment: 'Pix',
+    },
+    {
+      id: 2,
+      date: '14/08/24',
+      price: '13',
+      payment: 'Dinheiro',
+    },
+    {
+      id: 3,
+      date: '14/08/24',
+      price: '140',
+      payment: 'Crédito',
+    },
+  ];
+
+  addItem(price: any) {
+    this.items.push({ id: 2, date: '14/08/24', price: price, payment: 'Pix' });
+    this.priceValue = '';
+  }
+  mask() {
+    function preco(preco: string) {
+      preco = preco.replace(/\D/g, '');
+      preco = (Number(preco) / 100).toFixed(2).replace('.', ',');
+      return preco;
+    }
+    this.priceValue = 'R$ ' + preco(this.priceValue);
+  }
 }
